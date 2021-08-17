25a4b7ed3673a5fa5ce2a3327202181fed0341e5
Anish Sebastian <anishst@hotmail.com>
2021-08-17 13:31:53 -0400

    blog endpoints

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # FastAPI REST App with MongoDB
 
-## Run Options
-
-- run from command line: ``` uvicorn main:app --reload --host=0.0.0.0```
+## Run Locally - Linux
+- create virtual env: ```python3 -m venv venv```
+- activate: ```source venv/bin/activate```
+- install required dependencies: ```pip install -r requirements.txt```
+- run app from command line: ``` uvicorn main:app --reload --host=0.0.0.0```
 
 ## Resources
 - [FastAPI Github](https://github.com/tiangolo/fastapi)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional, Text
+from datetime import datetime
 
 app = FastAPI()
 
@@ -9,7 +10,41 @@ app = FastAPI()
 def read_root():
     return {"message": "Welcome to my very Basic FastAPI!"}
 
+postdb = []
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# post model
+class Post(BaseModel):
+    id: int
+    title: str
+    author: str
+    content: Text
+    created_at: datetime = datetime.now()
+    published_at: datetime
+    published: Optional[bool] = False
+
+# route to view all posts.
+@app.get("/blog")
+def get_posts():
+    return postdb
+
+@app.post("/blog")
+def add_post(post: Post):
+    postdb.append(post.dict())
+    return postdb[-1]
+
+@app.get("/blog/{post_id}")
+def get_post(post_id: int):
+    post = post_id - 1
+    return postdb[post]   
+
+# update post
+@app.post("/blog/{post_id}")
+def update_post(post_id: int, post: Post):
+    postdb[post_id] = post
+    return {"message": "Post has been updated succesfully"} 
+
+# delete post
+@app.delete("/blog/{post_id}")
+def delete_post(post_id: int):
+    postdb.pop(post_id-1)
+    return {"message": "Post has been deleted succesfully"}
